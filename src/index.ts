e7855dbb67e22405aa626e00402935d4ddf5f610
jrusso1020 <jdrusso1020@gmail.com>
2022-12-28 14:57:54 -0500

    feat(calculatetaxamount): adding a basic function to calculate the tax amount based on tax brackets
    Adding the root function that is going to be responsible for calcualting progressive income tax
    amount due based on the amount passed in and the tax brackets passed in

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+// This simple function is reponsible for calculating the of taxes owed based on the passed in amount and tax brackets
+// Params:  amount[required]: the amount of money to use for calculating taxes owed (i.e. income amount)
+//          brackets[required]: the tax brackets to use for calculating taxes
+export const calculateTaxAmount = (
+  amount: number,
+  brackets: { maxAmount: number; rate: number }[]
+) => {
+  let taxAmount = 0;
+
+  for (let index = 1; index < brackets.length; index++) {
+    if (amount > brackets[index].maxAmount) {
+      taxAmount +=
+        (brackets[index].maxAmount - brackets[index - 1].maxAmount) *
+        brackets[index].rate;
+    } else {
+      taxAmount +=
+        (amount - brackets[index - 1].maxAmount) * brackets[index].rate;
+      return taxAmount;
+    }
+  }
+
+  return taxAmount;
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,59 @@
-import { myPackage } from '../src';
+import { calculateTaxAmount } from '../src';
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  describe('test calculateTaxAmount across brackets', () => {
+    const brackets = [
+      { maxAmount: 0, rate: 0 },
+      { maxAmount: 10275, rate: 0.1 },
+      { maxAmount: 41775, rate: 0.12 },
+      { maxAmount: 89075, rate: 0.22 },
+      { maxAmount: 170050, rate: 0.24 },
+      { maxAmount: 215950, rate: 0.32 },
+      { maxAmount: 539900, rate: 0.35 },
+      { maxAmount: Infinity, rate: 0.37 },
+    ];
 
-      const result = myPackage(message);
+    it('first bracket', () => {
+      expect(calculateTaxAmount(500, brackets)).toBe(500 * 0.1);
+    });
 
-      expect(result).toMatch(message);
+    it('second bracket', () => {
+      expect(calculateTaxAmount(20000, brackets)).toBe(
+        (20000 - 10275) * 0.12 + 1027.5
+      );
+    });
+
+    it('third bracket', () => {
+      expect(calculateTaxAmount(50000, brackets)).toBe(
+        (50000 - 41775) * 0.22 + 4807.5
+      );
+    });
+
+    it('fourth bracket', () => {
+      expect(calculateTaxAmount(100000, brackets)).toBe(
+        (100000 - 89075) * 0.24 + 15213.5
+      );
+    });
+
+    it('fifth bracket', () => {
+      expect(calculateTaxAmount(200000, brackets)).toBe(
+        (200000 - 170050) * 0.32 + 34647.5
+      );
+    });
+
+    it('sixth bracket', () => {
+      expect(calculateTaxAmount(500000, brackets)).toBe(
+        (500000 - 215950) * 0.35 + 49335.5
+      );
+    });
+
+    it('seventh bracket', () => {
+      expect(calculateTaxAmount(750000, brackets)).toBe(
+        (750000 - 539900) * 0.37 + 162718
+      );
+      expect(calculateTaxAmount(7500000, brackets)).toBe(
+        (7500000 - 539900) * 0.37 + 162718
+      );
     });
   });
 });
